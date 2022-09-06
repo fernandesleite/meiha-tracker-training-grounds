16af913963795e404f7f3d619f795b4025d5a7d5
Bruno Fernandes <brunofdsleite@gmail.com>
2022-09-05 23:54:43 -0300

    add constants class

---
+++ app/src/main/java/com/moviedb/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.moviedb.util
+
+class Constants {
+    companion object {
+        const val FULL_IMAGE_IRL = "https://image.tmdb.org/t/p/original"
+        const val API_URL = "https://api.themoviedb.org/3/"
+        const val SCHEME = "https"
+        const val DATE_YEAR_MONTH_DAY = "yyyy-MM-dd"
+        const val DATE_MONTH_YEAR = "MMM yyyy"
+        const val DATE_YEAR = "yyyy"
+    }
+}
